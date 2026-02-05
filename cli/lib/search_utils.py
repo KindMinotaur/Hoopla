061ccf8f5c1e5e298047fe541aa61bb930b43f60
KindMinotaur <shaun.belknap@gmail.com>
2026-02-04 19:07:38 -0600

    Refactored code

--- cli/lib/search_utils.py
+++ cli/lib/search_utils.py
@@ -7,6 +7,7 @@ PROJECT_ROOT = os.path.dirname(os.path.dirname(os.path.dirname(__file__)))
 DATA_PATH = os.path.join(PROJECT_ROOT, "data", "movies.json")
 STOPWORDS_PATH = os.path.join(PROJECT_ROOT, "data", "stopwords.txt")
 
+
 def load_movies() -> list[dict]:
     with open(DATA_PATH, "r") as f:
         data = json.load(f)

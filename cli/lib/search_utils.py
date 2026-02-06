caa226e7ef09e3d46211bc04b91c436f5ba926ad
KindMinotaur <shaun.belknap@gmail.com>
2026-02-05 18:50:22 -0600

    Build Inverted Index class

--- cli/lib/search_utils.py
+++ cli/lib/search_utils.py
@@ -7,6 +7,8 @@ PROJECT_ROOT = os.path.dirname(os.path.dirname(os.path.dirname(__file__)))
 DATA_PATH = os.path.join(PROJECT_ROOT, "data", "movies.json")
 STOPWORDS_PATH = os.path.join(PROJECT_ROOT, "data", "stopwords.txt")
 
+CACHE_DIR = os.path.join(PROJECT_ROOT, "cache")
+
 
 def load_movies() -> list[dict]:
     with open(DATA_PATH, "r") as f:
@@ -17,3 +19,4 @@ def load_movies() -> list[dict]:
 def load_stopwords() -> list[str]:
     with open(STOPWORDS_PATH, "r") as f:
         return f.read().splitlines()
+
